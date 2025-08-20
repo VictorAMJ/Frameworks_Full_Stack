512693bc46ce03149b3cd88d2acb6057d9276ea6
Victor <victor.jesus@aluno.faculdadeimpacta.com.br>
2025-08-20 17:07:17 -0300

    api completa

--- api_atividade/app.py
+++ api_atividade/app.py
@@ -15,6 +15,9 @@ usuarios = [
     }
 ]
 
+
+
+'''FORMA CONVENCIONAL
 @app.route("/usuario", methods=["POST"])
 def criar_usuario():
     novo_usuario = request.json
@@ -23,6 +26,26 @@ def criar_usuario():
             return jsonify({"erro": "ID já existente."}), 400
     usuarios.append(novo_usuario)
     return jsonify(novo_usuario), 201
+'''
+
+#forma de ID automático:
+@app.route("/usuario", methods=["POST"])
+def criar_usuario():
+    novo_usuario = request.json
+    
+    #1. cria uma lista com os IDs dos usuários: [usuario["id"] for usuario in usuarios]
+    #2. max(...,default=0) +1: pega o maior ID da lista e caso a lista estiver vazia, será 0, e adiciona +1 caso ja exista outros.
+    novo_id = max([usuario["id"] for usuario in usuarios], default=0) +1
+
+    novo_usuario = {
+        "id": novo_id,
+        "nome": novo_usuario["nome"],
+        "email": novo_usuario["email"]
+    }
+    usuarios.append(novo_usuario)
+    return jsonify(novo_usuario), 201
+
+    #OBS: no postman, use apenas o nome e email
 
 
 
@@ -40,6 +63,7 @@ def listar_usuarios_id(idUsuario):
 
 
 
+'''PUT CONVENCIONAL
 @app.route("/usuario/<int:idUsuario>", methods=["PUT"])
 def atualizar_usuario(idUsuario):
     for usuario in usuarios:
@@ -50,7 +74,21 @@ def atualizar_usuario(idUsuario):
             usuario["nome"] = usuario_existente["nome"]
             usuario["email"] = usuario_existente["email"]
 
-            return jsonify(usuario_existente)
+            return jsonify(usuario)
+    return jsonify({"erro": "Não foi possível atualizar o usuário."}), 404
+'''
+
+#COM ID AUTOMATICO:
+@app.route("/usuario/<int:idUsuario>", methods=["PUT"])
+def atualizar_usuario(idUsuario):
+    for usuario in usuarios:
+        if usuario['id'] == idUsuario:
+            usuario_existente = request.json
+            
+            usuario["nome"] = usuario_existente["nome"]
+            usuario["email"] = usuario_existente["email"]
+            
+            return jsonify(usuario)
     return jsonify({"erro": "Não foi possível atualizar o usuário."}), 404
 
 
@@ -59,12 +97,9 @@ def atualizar_usuario(idUsuario):
 def deletar_usuario(idUsuario):
     for usuario in usuarios:
         if usuario['id'] == idUsuario:
-            usuario["id"].remove(usuario)
+            usuarios.remove(usuario)
             return jsonify({"mensagem": "Usuário deletado com sucesso!"}), 200
     return jsonify({"erro": "Usuário não encontrado."}), 404
-
-
-
 
 
 if __name__ == "__main__":
