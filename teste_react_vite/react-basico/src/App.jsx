e6c8080cd5ba42103649479d8ca697d894e729b6
Victor <victor.jesus@aluno.faculdadeimpacta.com.br>
2025-09-25 14:17:06 -0300

    add arquivos

---
+++ teste_react_vite/react-basico/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css" // importa o CSS
+
+// Componente App: função que retorna o conteúdo da interface
+function App() {
+    return (
+        <div>
+            <h1>Olá, React feito do zero!</h1>
+            <h2>Consegui fazer funcionar hehe</h2>
+            <p>XDDDDDD</p>
+        </div>
+    )
+}
+
+export default App
+// Exporta o componente App para ser usado em main.jsx
